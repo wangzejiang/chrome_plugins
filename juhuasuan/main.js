1853324803ed0e27452d8fee20104b8f9814ee97
ff <31499763+wangzejiang@users.noreply.github.com>
2019-05-09 21:30:16 +0800

    111

---
+++ juhuasuan/main.js
@@ -0,0 +1,9 @@
+var m = new Map();
+m.set("593087590098_抛壳版-98K_标配（送赠品：1万弹+倍镜+信号枪+标靶）",100);
+
+function initJHS(){
+	var id = $("div[label='商品ID：']").html();
+	console.log(id);
+	window.setTimeout("initJHS()",1000);
+}
+window.setTimeout("initJHS()",1000);
