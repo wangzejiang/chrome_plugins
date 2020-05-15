6cc17e042488bf287d98e74c9f1a81f49efef1bf
ff <339811501@qq.com>
2020-05-16 01:06:52 +0800

    111
    111

--- ztc/main3.js
+++ ztc/main3.js
@@ -1,12 +1,12 @@
 function init3(){
-	var mx = getMx("J_mx_","_cost_");
+	var mx = getMx("J_manage-adgroup-detail-list_cost_");
 	//console.log("zejiang init3 ren qun-->"+mx.length);
 	if(mx.length==0){
 		window.setTimeout("init3()",1000);
 		return;
 	}
-	var total_price = getMx("J_mx_","_total_cost");
-	var total_count = getMx("J_mx_","_total_transactionshippingtotal");
+	var total_price = getMx("J_manage-adgroup-detail-list_total_cost");
+	var total_count = getMx("J_manage-adgroup-detail-list_total_transactionshippingtotal");
 	var avg = val23(total_price.text()) / val23(total_count.text());
 	if(avg==0){
 		window.setTimeout("init3()",1000);
@@ -17,11 +17,13 @@ function init3(){
 	}else{
 		$('.gray-dark').eq(0).append("<font id='zejiang_avg' color='red'>"+avg+"</font>");
 	}
+	console.log("zejiang init3 ren qun-->"+mx.length);
 	mx.each(function(){
 		var cost = getConst3($(this).text());
 		var id = $(this).attr('id');
-		var wid = id.split('_')[4];
+		var wid = id.split('_')[3];
 		var newid = "zejiang_"+wid;
+		console.log("zejiang init3 ren qun id-->"+wid);
 		var total = getTransactionshippingtotal3(wid);
 		var rs = cost / total;
 		rs = rs.toFixed(2);
@@ -44,21 +46,19 @@ function init3(){
 	window.setTimeout("init3()",2000);
 }
 function getImpression3(id){
-	var text1 = "J_mx_";
-	var text2 = "_impression_"+id;
-	return $("td[id^='"+text1+"'][id$='"+text2+"']");
+	var text1 = "J_manage-adgroup-detail-list_impression_"+id;
+	return $("td[id='"+text1+"']");
 }
-function getMx(text1,text2){
-	return $("td[id^='"+text1+"'][id*='"+text2+"']");
+function getMx(text1){
+	return $("td[id^='"+text1+"']");
 }
 function getConst3(val){
 	var text = val23(val);
 	return text;
 }
 function getTransactionshippingtotal3(id){
-	var text1 = "J_mx_";
-	var text2 = "_transactionshippingtotal_"+id;
-	var text = $("td[id^='"+text1+"'][id$='"+text2+"']").text();
+	var text1 = "J_manage-adgroup-detail-list_transactionshippingtotal_"+id;
+	var text = $("td[id='"+text1+"']").text();
 	return val23(text);
 }
 function val23(val){
